48f3139b60cb1bf879c9491953fd891cd26f31d6
Кирилл Соловьев <k.solovev994@yandex.ru>
2023-02-20 23:29:27 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
 import sys
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
@@ -9,8 +8,7 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
-        self.loadTable('coffee.sqlite')
+        self.change_to_read()
 
     def loadTable(self, table_name):
         con = sqlite3.connect(table_name)
@@ -24,6 +22,42 @@ class MyWidget(QMainWindow):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
+        con.close()
+
+    def change_to_read(self):
+        uic.loadUi('main.ui', self)
+        self.loadTable('coffee.sqlite')
+        self.write_b.clicked.connect(self.change_to_write)
+
+    def change_to_write(self):
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.loadTable('coffee.sqlite')
+        self.read_b.clicked.connect(self.change_to_read)
+        self.add_b.clicked.connect(self.add)
+        self.save_b.clicked.connect(self.save)
+
+    def save(self):
+        data = []
+        for i in range(self.tableWidget.rowCount()):
+            row = []
+            for j in range(self.tableWidget.columnCount()):
+                item = self.tableWidget.item(i, j)
+                if item is not None:
+                    row.append(item.text())
+            data.append(tuple(row))
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        try:
+            cur.execute("""DELETE FROM coffee_data""")
+            cur.executemany(f"""INSERT INTO coffee_data VALUES (?, ?, ?, ?, ?, ?, ?)""", data)
+            con.commit()
+        except Exception:
+            pass
+        con.close()
+
+    def add(self):
+        self.tableWidget.setRowCount(
+            self.tableWidget.rowCount() + 1)
 
 
 if __name__ == '__main__':
